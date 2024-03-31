26d841f25c2b6c233edcb48aecfe0edf03ec7a09
Indrek Mannik <119368421+Inc21@users.noreply.github.com>
2024-03-31 22:59:21 +0100

    Add .vscode/settings.json, .gitignore, package.json, and index.js files

---
+++ index.js
@@ -0,0 +1,22 @@
+import OpenAI from 'openai';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const openai = new OpenAI({
+    apiKey: process.env.API_KEY,
+  });
+
+async function main() {
+const completion = await openai.chat.completions.create({
+    messages: [
+    { role: 'system', content: 'You are helpful assistant' },
+    { role: 'user', content: 'What is the capital of Ireland' },
+    ],
+    max_tokens: 5,
+    model: 'gpt-3.5-turbo',
+});
+
+console.log(completion.choices[0].message.content);
+}
+
+main();
